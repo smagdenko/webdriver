9bd2b0fb1433d30290a2dc557ab0bddfaa7cc2a6
smagdenko <magdenko_ne@ukr.net>
2019-06-19 17:31:40 +0300

    Change the xpath for look for all states

---
+++ CheckGeoZones_attempt_3.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+
+def login():
+
+    driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones")
+    driver.find_element_by_name("username").send_keys("admin")
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_name("login").click()
+
+
+def test_sort():
+
+    login()
+    countries = driver.find_elements_by_xpath(".//*[@class='dataTable']//td[3]/a")
+
+    for i in range(len(countries)):
+        len(countries)
+        countries[i].click()
+        # zones = driver.find_elements_by_xpath(".//*[@class='dataTable']"
+        #                                       "//td[3]//select[contains(@name,'zones')]")
+
+        select = driver.find_elements_by_xpath(".//*[@id='table-zones']//tr[2]//td[3]//option[@value !='']")
+        ls_countries = []
+        for element in select:
+            ls_countries.append(element.text)
+        sorted_countries = sorted(ls_countries)
+        assert sorted_countries == ls_countries
+        driver.back()
+        countries = driver.find_elements_by_xpath(".//*[@class='dataTable']//td[3]/a")
+
