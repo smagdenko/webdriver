4a36a8479a4f1222272a4d92f283f7cbc40cf33c
smagdenko <magdenko_ne@ukr.net>
2019-06-21 09:20:41 +0300

    Changed the xpath for selects of all zones

---
+++ GeoZones_attempt_4.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+
+def login():
+
+    driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones")
+    driver.find_element_by_name("username").send_keys("admin")
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_name("login").click()
+
+
+def test_sort():
+
+    login()
+    countries = driver.find_elements_by_xpath(".//*[@class='dataTable']//td[3]/a")
+
+    for i in range(len(countries)):
+        len(countries)
+        countries[i].click()
+        select = driver.find_elements_by_xpath("//form[contains(@method,'post')]//td[3]//option[@selected='selected']")
+        ls_countries = []
+        for element in select:
+            ls_countries.append(element.text)
+        sorted_countries = sorted(ls_countries)
+        assert sorted_countries == ls_countries
+        driver.back()
+        countries = driver.find_elements_by_xpath(".//*[@class='dataTable']//td[3]/a")
